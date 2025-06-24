09995a1ea66dfc134909abdb739b2d28d8c58cba
qkdr <kskwkodh@gmail.com>
2025-06-24 16:34:34 +0300

    Lord.lua

--- Lord.lua
+++ Lord.lua
@@ -1,6 +1,6 @@
 -- جدول فيه معرفات المابات وروابط السكربتات الخاصة بها
 local games = {
-    [15438487904] = "https://raw.githubusercontent.com/DanielHubll/DanielHubll/refs/heads/main/Aimbot%20Mobile",
+    [15438487904] = "https://raw.githubusercontent.com/DanielHubll/DanielHubll/refs/heads/main/Aimbot%20Mobile"
 }
 
 local defaultScript = "https://raw.githubusercontent.com/qkdr/Thronix/refs/heads/main/Thronix.lua"
