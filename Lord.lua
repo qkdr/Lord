136c592e20745cad2335a103110e61c8b8909057
qkdr <kskwkodh@gmail.com>
2025-06-24 16:38:11 +0300

    Lord.lua

--- Lord.lua
+++ Lord.lua
@@ -1,14 +1,11 @@
--- جدول فيه معرفات المابات وروابط السكربتات الخاصة بها
 local games = {
-    [15438487904] = "https://raw.githubusercontent.com/DanielHubll/DanielHubll/refs/heads/main/Aimbot%20Mobile"
+    [15438487904] = "https://raw.githubusercontent.com/DanielHubll/DanielHubll/main/Aimbot%20Mobile.lua"
 }
 
-local defaultScript = "https://raw.githubusercontent.com/qkdr/Thronix/refs/heads/main/Thronix.lua"
-
+local defaultScript = "https://raw.githubusercontent.com/qkdr/Thronix/main/Thronix.lua"
 
 local scriptURL = games[game.PlaceId] or defaultScript
 
 getgenv().uiSize = UDim2.fromOffset(580, 400)
-
 
 loadstring(game:HttpGet(scriptURL))()
